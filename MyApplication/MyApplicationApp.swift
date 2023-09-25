0d9199cc81214ec84a6d6e80e815c4bcb4d7e1a0
tranhuyducseven <tranhuyducseven@gmail.com>
2023-09-25 10:34:02 +0700

    Initial Commit

---
+++ MyApplication/MyApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApplicationApp.swift
+//  MyApplication
+//
+//  Created by rosen on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
